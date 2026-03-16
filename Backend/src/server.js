2e79ba9ffc6361ea0ca3f8fc75e77d38dddcd798
aditya-pathak-codes <thepathakadityax@gmail.com>
2026-03-16 23:30:43 +0530

    mongodb connected

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,9 +1,10 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import path from 'path';
-import authRoutes from './routes/auth.routes.js';
-import messageRoutes from './routes/message.routes.js';
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
 
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -12,16 +13,22 @@ const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
+// middleware
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-//  make ready for deployment
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
-    app.get('*', (_, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
+// production deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-
-app.listen(PORT, () => console.log('Server is running on port '+ PORT));
+app.listen(PORT, async () => {
+  console.log(`Server running on port ${PORT}`);
+  await connectDB();
+});
