2c0cce6f47adb58fc8e5df4026d6def0161b4398
aditya-pathak-codes <thepathakadityax@gmail.com>
2026-03-16 23:31:36 +0530

    authentication in working...

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,11 +1,24 @@
 export const signup = async (req, res) => {
-    res.send("Signup endpoint");
-};
+    const { fullName, email, password } = req.body;
 
-export const login = async (req, res) => {
-    res.send("Login endpoint");
-};
+    try {
+        if (!fullName || !email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters long" });
+        }
 
-export const logout = async (req, res) => {
-    res.send("Logout endpoint");
+        // check if emails valid: regex for email validation
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ message: "Invalid email format" });
+        }
+
+
+    }
+     catch(error) {
+        console.error("Error during signup:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
 };
