a0284e1eab7bb148d2dcf9032f36969ccd7c204c
aditya-pathak-codes <thepathakadityax@gmail.com>
2026-03-22 22:34:32 +0530

    rate limitng done using arcjet

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -116,3 +116,4 @@ export const updateProfile = async (req, res) => {
     }
 };
     
+
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -4,7 +4,7 @@ import {protectRoute} from '../middlewares/auth.middlewares.js';
 const router = express.Router();
 
 router.post("/signup", signup);
-router.post("/login", login);
+router.post("/login", arcjetProtection, login);
 router.post("/logout", logout);
 
 router.put("/update-profile", protectRoute, updateProfile);
